5e5777f2f1d113ed08dd3095e01cf2614fb2b1fa
dohoonkim <mianhe89@gmail.com>
2024-06-21 08:53:08 +0900

    git init

--- src/main/java/com/sparta/shoppingmall/cart/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.shoppingmall.cart.repository;
-
-import com.sparta.shoppingmall.cart.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
-}
